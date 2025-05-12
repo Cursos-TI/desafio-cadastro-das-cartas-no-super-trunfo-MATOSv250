71bb02ed5a03fd7992b89cf380343a053b715e86
MATOSv250 <vdesouza711@gmail.com>
2025-05-12 16:33:14 +0000

    fiz o printf de São paulo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,19 +11,43 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     char A , B;
     char A01 , B02 [40];
-    char São paulo, Rio de janeiro [50];
+    char São paulo , Rio de janeiro [50];
     int 1232500 , 6748000 ;
     float 1521.11 km² , 1200.25 km²;
     float 699.28 bilhões de reais , 300.50 bilhões de reais;
     int 50,30;
-    
+
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
+    printf("Desafio super triunfo\n");
+
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    printf("Aqui as cartas dísponiveis\n");
+
     
     // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Estado: %c \n - Código: \n", A );
 
-    return 0;
+    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
+    printf("Código da carta: %s \n", A01);
+
+    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Nome da cidade: %s \n", São paulo);
+
+    
+    printf("População: %f \n", 12325000);
+
+    
+    printf("Área em km²:%f \n", 1521.11 km²);
+
+    
+    printf("PIB: %f \n", 699.28 bilhões de reais);
+
+    
+    printf("Número de pontos turísticos: %f \n", 50);
+
+     return 0; 
+     
+
+
 }
